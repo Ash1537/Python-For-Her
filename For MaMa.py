9e2773889fd78f16a2ea28cc0dbdaca6aa04e0e9
Ash <99724162+Ash1537@users.noreply.github.com>
2024-04-01 00:30:10 +0630

    Add files via upload

---
+++ For MaMa.py
@@ -0,0 +1,32 @@
+import turtle
+turtle.bgcolor("black") 
+# Creating a turtle object(pen)
+pen = turtle.Turtle()
+pen.pencolor("red")
+# Defining a method to draw curve
+def curve():
+ for i in range(200):
+  pen.right(1)
+  pen.forward(1) 
+# Defining method to draw a full heart
+def heart(): 
+ pen.fillcolor('red')
+ pen.begin_fill()
+ pen.left(140)
+ pen.forward(113)
+ curve()
+ pen.left(120)
+ curve()
+ pen.forward(112)
+ pen.end_fill()
+# Defining method to write text
+def txt():
+ pen.up()
+ pen.setpos(-68, 95)
+ pen.down()
+ pen.color('white')
+ pen.write("I LOVE YOU JUST YOU MAMAA <3", font=( "Verdana", 8, "bold"))
+heart()
+txt()
+
+pen.ht()
